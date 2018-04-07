d97f94137cd71d4fbbc22fc6200a5ff88ac64077
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-04-07 00:29:29 +0000

    Done.

--- lib/play.rb
+++ lib/play.rb
@@ -1,4 +1,3 @@
-# Helper Methods
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -7,38 +6,37 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def input_to_index(user_input)
-  user_input.to_i - 1
+def input_to_index(input)
+input.to_i - 1
 end
 
-def move(board, index, current_player = "X")
-  board[index] = current_player
-end
-
-def position_taken?(board, location)
-  board[location] != " " && board[location] != ""
+def move(board, index, value = "X")
+  board[index] = value
 end
 
 def valid_move?(board, index)
-  index.between?(0,8) && !position_taken?(board, index)
+  index.between?(0, 8) && !(position_taken?(board, index))
+end
+
+def position_taken?(board, index)
+  board[index] == "X" || board[index] == "O"
 end
 
 def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip
   index = input_to_index(input)
-  if valid_move?(board, index)
+  
+  if valid_move?(board,index)
     move(board, index)
     display_board(board)
-  else
-    turn(board)
+  else turn(board)
   end
 end
 
-# Define your play method below
 def play(board)
 counter = 0
-while counter < 9
+while(counter < 9)
   turn(board)
   counter += 1
 end
